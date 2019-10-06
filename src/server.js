b7a0e8f478f108af591c1cbe8c5d00be42373359
Pedro Soares <pedropaulos.1998@gmail.com>
2019-10-06 15:37:58 -0300

    update routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 
 //  GET, POST, PUT, DELETE
 
-app.post('/users', (req, res) => {
-  return res.json({ messege: "Hello OmniStack" });
-});
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route params (para edição, delete
+//req.bady = Acessar corpo da requisição (criação, edição)
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
