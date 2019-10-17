e054aa182f2d2ce2948407a63cd34a645841410b
Pedro Soares <pedropaulos.1998@gmail.com>
2019-10-16 22:58:11 -0300

    controllers add (spot & dashboard)

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,16 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
-routes.post('/session', SessionController.store);
-routes.post('/spot', upload.single('thumbnail'), SpotController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
