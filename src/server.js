60449e869cf906572047e7b32013e190a8eb2371
Pedro Soares <pedropaulos.1998@gmail.com>
2019-10-21 15:05:13 -0300

    performatic changes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
+require('dotenv').config();
+
+const dbConnect = require('./db');
 const express = require('express');
-const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
-mongoose.connect('mongodb://PedroSoares:1234@omnistack-shard-00-00-jm4st.mongodb.net:27017,omnistack-shard-00-01-jm4st.mongodb.net:27017,omnistack-shard-00-02-jm4st.mongodb.net:27017/OmniStack?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+dbConnect();
 
 app.use(express.json());
 app.use(routes);
