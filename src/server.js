25e0922173765605be11ead571f7e4ca91b31cfc
Pedro Soares <pedropaulos.1998@gmail.com>
2020-01-02 16:28:24 -0300

    uploads files

--- src/server.js
+++ src/server.js
@@ -3,11 +3,17 @@ require('dotenv').config();
 const dbConnect = require('./db');
 const express = require('express');
 const routes = require('./routes');
+
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
 dbConnect();
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
